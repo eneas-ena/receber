0cb24c58117b83ceccec78a6f80b982556416969
eneas-ena <eneaspr@gmail.com>
2026-07-06 09:15:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "receber-v3";
+const CACHE = "receber-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
